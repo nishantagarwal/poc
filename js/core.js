4ac3a9f8917fb99c5f0657aeb8ab0e23c2b5e8ac
Nishant Agarwal <agarwal.nishant07@gmail.com>
2015-01-14 21:33:11 +0100

    Created index html

---
+++ js/core.js
@@ -0,0 +1,22 @@
+﻿$(function () {
+    //search-list
+    $('form.search input.search').each(function () {
+        if ($(this).attr('data-list')) {
+            $(this).keyup(function () {
+                var value = $(this).val();
+                var list = "." + $(this).attr('data-list');
+                if (value.length < 3) {
+                    $(list).children().show();
+                } else {
+                    $(list).children().hide();
+                    $(list).children().each(function () {
+                        var tags = $(this).attr('data-tags');
+                        if (tags.indexOf(value) > -1) {
+                            $(this).show();
+                        }
+                    });
+                }
+            });
+        }
+    });
+});
